bc89f5b0eef329f33d1652069277dacfc18f53ba
xhc2008 <13913016861@163.com>
2025-09-05 13:30:01 +0800

    feat: 后台启动

--- start.bat
+++ start.bat
@@ -1,2 +1,8 @@
-python main.py
-pause
+@echo off
+echo Set WshShell = CreateObject("WScript.Shell") > temp.vbs
+echo WshShell.Run "python main.py", 0, False >> temp.vbs
+echo Set WshShell = Nothing >> temp.vbs
+start temp.vbs
+timeout /t 1 /nobreak >nul
+del temp.vbs
+exit
